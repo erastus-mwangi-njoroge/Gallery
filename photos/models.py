41511e62dde8dfb6e21c51b29601c90b5f8eba34
Erastus Njoroge <erastus.njoroge@student.moringaschool.com>
2021-10-08 22:24:17 +0300

    makemigrations

--- photos/models.py
+++ photos/models.py
@@ -9,10 +9,10 @@ class Category(models.Model):
 
 class Image(models.Model):
     name = models.CharField(max_length=30)
-    image = models.ImageField(upload_to='images')
+    image = models.ImageField(upload_to='images', default=None)
     description = models.TextField()
-    location = models.ForeignKey(Location)
-    category = models.ForeignKey(Category)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE,)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE,)
     uploader = models.CharField(max_length=20)
     time = models.DateTimeField(auto_now_add=True)
 
