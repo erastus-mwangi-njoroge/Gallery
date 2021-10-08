9304cb9b9057a4d62b98902a7e8328fddd10f7a0
Erastus Njoroge <erastus.njoroge@student.moringaschool.com>
2021-10-08 22:01:50 +0300

    add a class category  in the app model

--- photos/models.py
+++ photos/models.py
@@ -1,4 +1,8 @@
 from django.db import models
 
 class Location(models.Model):
-    name = models.CharField(max_length=15)
+    name = models.CharField(max_length=20)
+
+    
+class Category(models.Model):
+    name = models.CharField(max_length=20)
