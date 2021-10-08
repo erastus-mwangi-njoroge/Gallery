735cdc9f42a4377467731bc0291560e6bc1ad52e
Erastus Njoroge <erastus.njoroge@student.moringaschool.com>
2021-10-09 00:58:13 +0300

    add functions for location model

--- photos/models.py
+++ photos/models.py
@@ -24,6 +24,16 @@ class Location(models.Model):
 class Category(models.Model):
     name = models.CharField(max_length=20)
 
+    def __str__(self):
+        return self.name
+
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
+
+
 class Image(models.Model):
     name = models.CharField(max_length=30)
     image = models.ImageField(upload_to='images', default=None)
