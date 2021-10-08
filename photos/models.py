ee7401fbe1328fae31f016e67acf17a2426fce00
Erastus Njoroge <erastus.njoroge@student.moringaschool.com>
2021-10-08 22:16:11 +0300

    add a class image in the app model

--- photos/models.py
+++ photos/models.py
@@ -3,6 +3,16 @@ from django.db import models
 class Location(models.Model):
     name = models.CharField(max_length=20)
 
-    
+
 class Category(models.Model):
     name = models.CharField(max_length=20)
+
+class Image(models.Model):
+    name = models.CharField(max_length=30)
+    image = models.ImageField(upload_to='images')
+    description = models.TextField()
+    location = models.ForeignKey(Location)
+    category = models.ForeignKey(Category)
+    uploader = models.CharField(max_length=20)
+    time = models.DateTimeField(auto_now_add=True)
+
