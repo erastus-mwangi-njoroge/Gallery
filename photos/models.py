2736054e9108d92402d76b35935c573274bc490f
Erastus Njoroge <erastus.njoroge@student.moringaschool.com>
2021-10-09 00:52:57 +0300

    add functions for location model

--- photos/models.py
+++ photos/models.py
@@ -2,7 +2,24 @@ from django.db import models
 
 class Location(models.Model):
     name = models.CharField(max_length=20)
+    
+    @classmethod
+    def get_locations(cls):
+        locations = Location.objects.all()
+        return locations
 
+    def __str__(self):
+        return self.name
+
+    @classmethod
+    def update_location(cls, id, value):
+        cls.objects.filter(id=id).update(image=value)
+
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
 
 class Category(models.Model):
     name = models.CharField(max_length=20)
