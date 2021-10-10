df1cf56c1b28760be80350e995bc0d45b91bb873
Erastus Njoroge <erastus.njoroge@student.moringaschool.com>
2021-10-11 02:38:27 +0300

    add to views.py

--- photos/views.py
+++ photos/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest,Http404
 from .models import Image,Location,Category
 
+def display_image(request):
+    imageResults= Image.objects.all()
+    return render(request,'index.html',{'Image':imageResults})
