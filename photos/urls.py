2fa2bb4b3c1b7125a74f937dce2738d60cf04e97
Erastus Njoroge <erastus.njoroge@student.moringaschool.com>
2021-10-13 02:11:31 +0300

    add search details

--- photos/urls.py
+++ photos/urls.py
@@ -3,7 +3,8 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns = [url(r'',views.display_image,name='images'),
+urlpatterns = [url(r'',views.display_image,name='imageResults'),
+                url(r'^search/', views.search_results, name='search_results'),
 ]
 
 if settings.DEBUG:
--- photos/views.py
+++ photos/views.py
@@ -2,6 +2,22 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest,Http404
 from .models import Location, Category, Image
 
+
 def display_image(request):
     imageResults= Image.objects.all()
     return render(request,"photoviewer/index.html",{'imageResults':imageResults})
+
+def search_results(request):
+
+    if 'photoling' in request.GET and request.GET["category"]:
+        search_term = request.GET.get("category")
+        searched_images = Image.search_by_category(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'photoviewer/search.html',{"message":message,"images": searched_images})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'photoviewer/search.html',{"message":message})
+
+
